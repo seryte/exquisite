f56d67fc32d3b7abcbadd33510589167a3d9161f
here~ <382643558@qq.com>
2023-12-04 14:41:06 +0800

    Update fake.js

--- fake.js
+++ fake.js
@@ -1,7 +1,30 @@
 var body = $response.body;
 var obj = JSON.parse(body);
+var currentDate = new Date();
 
-obj['data']['inlotTime'] = "2023-12-04 12:39:34";
+var year = currentDate.getFullYear();
+var month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+var day = ('0' + currentDate.getDate()).slice(-2);
+var hours = ('0' + currentDate.getHours()).slice(-2);
+var minutes = ('0' + currentDate.getMinutes()).slice(-2);
+var seconds = ('0' + currentDate.getSeconds()).slice(-2);
+
+var formattedDate = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+
+console.log(formattedDate);
+obj['data']['inlotTime'] = formattedDate;
+obj['data']['timeLengthStr'] = "0天1小时51分";
+obj['data']['preCash'] = 0;
+obj['data']['preCash'] = 0;
+obj['data']['cashStartTime'] = formattedDate;
+obj['data']['cashEndTime'] = formattedDate;
+obj['data']['cash'] = 0;
+obj['data']['inTime'] = formattedDate;
+obj['data']['outTime'] = formattedDate;
+
+
+
+
 body = JSON.stringify(obj);
 
 
