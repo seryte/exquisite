d2e9721280c932d33729b1cef54320cdd1b5fda3
here~ <382643558@qq.com>
2023-12-04 12:58:12 +0800

    Update fake.js

--- fake.js
+++ fake.js
@@ -1,9 +1,12 @@
 var body = $response.body;
 var obj = JSON.parse(body);
+console.log(body);
+console.log(123444);
+console.log(obj);
+
 
 obj['inlotTime']['data'] = "2023-12-04 12:39:34";
 body = JSON.stringify(obj);
 
-console.log(body);
 
 $done(body);
