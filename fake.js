99d8cbf3ba9a5f23da5c62c00db0b0cea66b51b5
here~ <382643558@qq.com>
2023-12-05 12:31:41 +0800

    Update fake.js

--- fake.js
+++ fake.js
@@ -1,6 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 var currentDate = new Date();
+var periodDetails = obj['data']['periodDetails']
 
 var year = currentDate.getFullYear();
 var month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
@@ -22,8 +23,12 @@ obj['data']['cash'] = 0;
 obj['data']['inTime'] = formattedDate;
 obj['data']['outTime'] = formattedDate;
 
-
-
+var new_periodDetails = periodDetails.forEach(detail => {
+  detail.amount = 0;
+  detail.explain = "阶段免停";
+});
+console.log(new_periodDetails);
+obj['data']['periodDetails'] = new_periodDetails;
 
 body = JSON.stringify(obj);
 
